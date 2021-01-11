cb05aa2fe58013007491ba1b57b33cf648471892
rko1985 <35703264+rko1985@users.noreply.github.com>
2021-01-11 07:34:40 -1000

    create explosion when collision

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,18 +2,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; //ok as long as this is the only script that loads scenes
 
 public class CollisionHandler : MonoBehaviour
 {
+    [Tooltip("In seconds")] [SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("FX prefab on player")] [SerializeField] GameObject deathFX;
 
     void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
+        deathFX.SetActive(true);
+        Invoke("ReloadScene", levelLoadDelay);
     }
 
     private void StartDeathSequence()
     {
         print("player dying");
         SendMessage("OnPlayerDeath");
+
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(1);
     }
 }
